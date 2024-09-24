a56dda6b857a41e5ccf977de6a708e005eeeea33
Minister124 <kushaltmg321@gmail.com>
2024-09-24 12:04:33 +0545

    blazor with tailwinf

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./wwwroot/index.html",
+    "./Pages/**/*.{razor,cshtml}",
+    "./Layout/**/*.{razor,cshtml}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
